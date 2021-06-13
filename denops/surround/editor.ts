1ae1fcb96a17d98e886c1e91c4d126465e5d458a
tayusa <37957375+tayusa@users.noreply.github.com>
2021-06-13 14:10:14 +0900

    Separate the files

---
+++ denops/surround/editor.ts
@@ -0,0 +1,36 @@
+import { Vim } from "https://deno.land/x/denops_std@v0.14/mod.ts";
+
+export class Editor {
+  private vim: Vim;
+
+  constructor(vim: Vim): void {
+    this.vim = vim;
+  }
+
+  getLine(row: number): string {
+    if (row === 0) {
+      return this.vim.call('getline', '.');
+    }
+    return this.vim.call('getline', row.toString());
+  }
+
+  setLine(row: number, line: string): void {
+    if (row === 0) {
+      this.vim.call('setline', '.', line);
+      return;
+    }
+    this.vim.call('setline', row.toString(), line);
+  }
+
+  getRow(): number {
+    return this.vim.call('line', '.');
+  }
+
+  getCol(): number {
+    return this.vim.call('col', '.');
+  }
+
+  nextNonBlank(row: number): number {
+    return this.vim.call('nextnonblank', row);
+  }
+}
